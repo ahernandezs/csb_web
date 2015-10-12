f62c57a01798bd5a4574353cfc9db3821ca8c36b
Jesus Macedo <macedo.rjpablo@gmail.com>
2015-10-12 12:55:46 -0500

    Created directive for passwords.

---
+++ SPA/app/scripts/directives/password.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('spaApp').directive('password', function(){
+   return {
+     require: 'ngModel',
+     link: function(scope, element, attrs, modelCtrl) {
+       modelCtrl.$parsers.push(function (inputValue) {
+           if (inputValue == undefined) return '';
+           var transformedInput = inputValue.replace(/[^a-zA-Z0-9]/g, '');
+           if (transformedInput!=inputValue) {
+              modelCtrl.$setViewValue(transformedInput);
+              modelCtrl.$render();
+           }
+
+           return transformedInput;
+       });
+     }
+   };
+});
