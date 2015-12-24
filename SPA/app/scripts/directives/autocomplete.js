127c7e1ca9aeb3bbf90b91213aa32d1087b423ef
Máximo Hdz <ahernandezs@anzen.com.mx>
2015-12-24 14:03:04 -0600

    issue fixex

--- SPA/app/scripts/directives/autocomplete.js
+++ SPA/app/scripts/directives/autocomplete.js
@@ -26,7 +26,7 @@ angular.module('spaApp')
             }
             if (scope.options.country) {
               opts.componentRestrictions = {
-                country: scope.options.country;
+                country: scope.options.country
               }
             }
           }
