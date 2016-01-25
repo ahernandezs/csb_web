dde7298ec487b6cb49a3186e677add2a672208c2
Jesus Macedo <macedo.rjpablo@gmail.com>
2016-01-25 12:33:37 -0600

    Added evaluation for IE inside the logout service.

--- SPA/app/scripts/services/users/user-service.js
+++ SPA/app/scripts/services/users/user-service.js
@@ -53,7 +53,41 @@ angular.module('spaApp')
 		};
 
 		this.logout = function() {
-			return $http( { cache: false, url: $rootScope.restAPIBaseUrl+'/logout', method: 'GET' } );
+			// detect IE
+			var IEversion = detectIE();
+
+			if (IEversion !== false)
+				return $http( { cache: false, url: $rootScope.restAPIBaseUrl+'/logout', method: 'GET', params: { 'time': new Date().getTime() } } );
+			else
+				return $http( { cache: false, url: $rootScope.restAPIBaseUrl+'/logout', method: 'GET' } );
+
+			/**
+			 * detect IE
+			 * returns version of IE or false, if browser is not Internet Explorer
+			 */
+			function detectIE() {
+				var ua = window.navigator.userAgent;
+
+				var msie = ua.indexOf('MSIE ');
+				// IE 10 or older => return version number
+				if (msie > 0)
+					return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+
+				var trident = ua.indexOf('Trident/');
+				// IE 11 => return version number
+				if (trident > 0) {
+					var rv = ua.indexOf('rv:');
+					return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+				}
+
+				var edge = ua.indexOf('Edge/');
+				// Edge (IE 12+) => return version number
+				if (edge > 0)
+					return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+
+				// other browser
+				return false;
+			};
 		};
 
 }]);
