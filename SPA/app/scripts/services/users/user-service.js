682eeeb49395a38d1a91409d4c9d57a64fd98d96
Jesus Macedo <macedo.rjpablo@gmail.com>
2016-01-25 13:27:51 -0600

    Moved browser detection to a new service. Modified received value from browser evaluation.

--- SPA/app/scripts/services/users/user-service.js
+++ SPA/app/scripts/services/users/user-service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('spaApp')
-	.service('userService', ['$http','$rootScope',function ($http, $rootScope) {
+	.service('userService', ['$http', '$rootScope', 'detectIE', function ($http, $rootScope, detectIE) {
 		this.preRegisterUser = function(clientId, folioId){
 			return $http({
 					url: $rootScope.restAPIBaseUrl+'/preregister',
@@ -53,41 +53,10 @@ angular.module('spaApp')
 		};
 
 		this.logout = function() {
-			// detect IE
-			var IEversion = detectIE();
-
-			if (IEversion !== false)
+			if ( detectIE.isIE() )
 				return $http( { cache: false, url: $rootScope.restAPIBaseUrl+'/logout', method: 'GET', params: { 'time': new Date().getTime() } } );
 			else
 				return $http( { cache: false, url: $rootScope.restAPIBaseUrl+'/logout', method: 'GET' } );
-
-			/**
-			 * detect IE
-			 * returns version of IE or false, if browser is not Internet Explorer
-			 */
-			function detectIE() {
-				var ua = window.navigator.userAgent;
-
-				var msie = ua.indexOf('MSIE ');
-				// IE 10 or older => return version number
-				if (msie > 0)
-					return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-
-				var trident = ua.indexOf('Trident/');
-				// IE 11 => return version number
-				if (trident > 0) {
-					var rv = ua.indexOf('rv:');
-					return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-				}
-
-				var edge = ua.indexOf('Edge/');
-				// Edge (IE 12+) => return version number
-				if (edge > 0)
-					return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-
-				// other browser
-				return false;
-			};
 		};
 
 }]);
