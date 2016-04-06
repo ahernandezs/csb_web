0b9e2101c9365efa35abb87e022d6416d2d83000
Jesus Macedo <macedo.rjpablo@gmail.com>
2016-04-06 12:55:11 -0500

    Modified service to detect IE, now returning ie version.

--- SPA/app/scripts/services/utils/detectIE.js
+++ SPA/app/scripts/services/utils/detectIE.js
@@ -5,26 +5,26 @@ angular.module('spaApp').service('detectIE', function() {
      * detect IE
      * returns true for any version of IE or false, if browser is not Internet Explorer
      */
-    this.isIE = function() {
+    this.detect = function() {
         var ua = window.navigator.userAgent;
 
         // IE 10 or older
         var msie = ua.indexOf('MSIE ');
         if (msie > 0)
-            return true;
+            return { 'ie' : true, 'version' : '10>' };
 
         // IE 11
         var trident = ua.indexOf('Trident/');
         if (trident > 0)
-            return true;
+            return { 'ie' : true, 'version' : 11 };
 
         // Edge
         var edge = ua.indexOf('Edge/');
         if (edge > 0)
-            return true;
+            return { 'ie' : true, 'version' : 12 };
 
         // other browser
-        return false;
+        return { 'ie' : false, 'version' : 0 };
     };
 
 });
